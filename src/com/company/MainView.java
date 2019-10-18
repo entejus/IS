5a9556cc1aeb8bbfcb9afa08bee94560f65a4bc8
Jakub <mielniczuk.jakub@gmail.com>
2019-10-18 15:14:35 +0200

    Running program in gui

--- src/com/company/MainView.java
+++ src/com/company/MainView.java
@@ -1,9 +1,80 @@
 package com.company;
 
 import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class MainView {
     private JButton importButton;
     private JButton exportButton;
     private JTable dataJTable;
+    private JPanel mainJPanel;
+
+
+    public MainView() throws IOException {
+        File readFile = new File("src/katalog.txt");
+
+        BufferedReader br = new BufferedReader(new FileReader(readFile));
+
+        String line;
+
+        String[] LABELS = {"Producent", "Przekątna", "Rozdzielczość", "Matryca", "?", "Procesor",
+                "L. rdzeni", "Cena", "RAM", "Pojemność", "Dysk", "Karta graficzna", "VRAM", "System", "Napęd"};
+
+        String[] FORMATSTYLES = {"| %-10s", "| %-10s", "| %-13s", "| %-11s", "| %-4s", "| %-9s", "| %-10s", "| %-5s",
+                "| %-5s", "| %-10s", "| %-5s", "| %-25s", "| %-5s", "| %-25s", "| %-8s |"};
+
+        for (int i = 0; i < LABELS.length; i++) {
+            System.out.printf(FORMATSTYLES[i], LABELS[i]);
+        }
+        System.out.println();
+        System.out.println("-------------------------------------------------------------------------------------------" +
+                "-----------------------------------------------------------------------------------------------");
+        while ((line = br.readLine()) != null) {
+            List<String> lineCells = splitToCells(line);
+
+            for (int i = 0; i < lineCells.size(); i++) {
+                System.out.printf(FORMATSTYLES[i], lineCells.get(i));
+            }
+            System.out.println();
+        }
+        System.out.println("-------------------------------------------------------------------------------------------" +
+                "-----------------------------------------------------------------------------------------------");
+    }
+
+
+    private static List<String> splitToCells(String line) {
+        List<String> cells = new ArrayList<>();
+
+        StringTokenizer st = new StringTokenizer(line, ";", true);
+
+        String lastToken = "";
+
+        while (st.hasMoreTokens()) {
+            String token = st.nextToken();
+            if (token.equals(lastToken) || (token.equals(";") && lastToken.equals(""))) {
+                cells.add("---");
+            } else if (!token.equals(";")) {
+                cells.add(token);
+            }
+            lastToken = token;
+        }
+
+        return cells;
+    }
+
+    public static void main(String[] args) throws IOException {
+        JFrame frame = new JFrame("Integracja Systemów - Jakub Mielniczuk");
+        frame.setContentPane(new MainView().mainJPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setResizable(true);
+    }
+
 }
